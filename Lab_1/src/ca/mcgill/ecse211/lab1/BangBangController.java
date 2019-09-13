30e85256d80618cd4b63b5e2d4045c5992cdfe68
Y.P <yp.cheng@hotmail.ca>
2019-09-13 14:02:12 -0400

    initial commit prototype

---
+++ Lab_1/src/ca/mcgill/ecse211/lab1/BangBangController.java
@@ -0,0 +1,57 @@
+package ca.mcgill.ecse211.lab1;
+
+import static ca.mcgill.ecse211.lab1.Resources.*;
+
+public class BangBangController extends UltrasonicController {
+	
+	private static int DISTANCE_BUFFER; //used to store the previous distance
+	
+	public BangBangController() {
+		LEFT_MOTOR.setSpeed(MOTOR_HIGH); // Start robot moving forward
+	    RIGHT_MOTOR.setSpeed(MOTOR_HIGH);
+	    LEFT_MOTOR.forward();
+	    RIGHT_MOTOR.forward();
+	}
+	
+	@Override
+	public void processUSData(int distance) {
+		
+		filter(distance);
+		
+		int distError = BAND_CENTER - readUSDistance();		
+
+		if (Math.abs(distError) <= BAND_WIDTH &&(-distError <= 2*DISTANCE_BUFFER)) {
+			LEFT_MOTOR.setSpeed(MOTOR_LOW);
+			RIGHT_MOTOR.setSpeed(MOTOR_LOW);
+			LEFT_MOTOR.forward();
+			RIGHT_MOTOR.forward();
+		}
+		else if (Math.abs(distError) <= BAND_WIDTH) {
+			LEFT_MOTOR.setSpeed(MOTOR_HIGH);
+			RIGHT_MOTOR.setSpeed(MOTOR_HIGH);
+			LEFT_MOTOR.forward();
+			RIGHT_MOTOR.forward();
+		}
+		else if (distError > 0) {
+			LEFT_MOTOR.setSpeed(MOTOR_LOW);
+			RIGHT_MOTOR.setSpeed(MOTOR_HIGH);
+			LEFT_MOTOR.forward();
+			RIGHT_MOTOR.forward();
+		}
+		else {
+			LEFT_MOTOR.setSpeed(MOTOR_HIGH);
+			RIGHT_MOTOR.setSpeed(MOTOR_LOW);
+			LEFT_MOTOR.forward();
+			RIGHT_MOTOR.forward();
+		}
+		
+		DISTANCE_BUFFER = this.distance;
+    
+	}
+
+	@Override
+	public int readUSDistance() {
+		return this.distance;
+		
+	}
+}

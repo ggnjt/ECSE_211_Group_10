6711ab75ba8ae9cbd6aa348273a9db535dfaa627
Elias <elias.alhomsi@mail.mcgill.ca>
2019-09-22 20:40:21 -0400

    finish the no correction

--- Lab_2/src/ca/mcgill/ecse211/lab2/Resources.java
+++ Lab_2/src/ca/mcgill/ecse211/lab2/Resources.java
@@ -23,7 +23,7 @@ public class Resources {
   /**
    * The robot width in centimeters.
    */
-  public static final double TRACK = 11.8;
+  public static final double TRACK = 11.57;
   
   /**
    * The speed at which the robot moves forward in degrees per second.

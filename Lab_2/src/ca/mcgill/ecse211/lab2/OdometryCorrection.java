5cf8a78e9cbcb08848cb650b02c80dd7499289cc
Elias <elias.alhomsi@mail.mcgill.ca>
2019-09-22 21:18:00 -0400

    add color detector

--- Lab_2/src/ca/mcgill/ecse211/lab2/OdometryCorrection.java
+++ Lab_2/src/ca/mcgill/ecse211/lab2/OdometryCorrection.java
@@ -1,24 +1,38 @@
 package ca.mcgill.ecse211.lab2;
 
 import static ca.mcgill.ecse211.lab2.Resources.*;
+import java.util.Arrays;
+import lejos.hardware.Sound;
+import lejos.robotics.SampleProvider;
 
 public class OdometryCorrection implements Runnable {
   private static final long CORRECTION_PERIOD = 10;
+  
+  // sensor
+  private SampleProvider sampleProvider = colorSensor.getRedMode();
+  private float[] sampleColor = new float[colorSensor.sampleSize()];
 
   /*
    * Here is where the odometer correction code should be run.
    */
   public void run() {
     long correctionStart, correctionEnd;
-
+    
+    int numberOfLines = 0;
+    boolean walkingOnWood = true;
+    
     while (true) {
       correctionStart = System.currentTimeMillis();
 
-      colorSensor.getColorIDMode();
-      if (colorSensor.getColorID() == 7) { // on wood
-
-      } else if (colorSensor.getColorID() == 1) {// on black line
-
+      sampleProvider.fetchSample(sampleColor, 0);
+      System.out.println(Arrays.toString(sampleColor));
+      if(sampleColor[0]  <  0.073 && walkingOnWood) {
+        Sound.beep();
+        numberOfLines++;
+        walkingOnWood = false;
+      }
+      else {
+        walkingOnWood = true;
       }
       // TODO Trigger correction (When do I have information to correct?)
 

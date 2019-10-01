370e136033e51238a324ddf626af33513100f361
Elias <elias.alhomsi@mail.mcgill.ca>
2019-09-30 20:04:42 -0400

    make it move 0.5 of a tile in case of an emergency

--- Lab_3/src/ca/mcgill/ecse211/lab3/RobotDriver.java
+++ Lab_3/src/ca/mcgill/ecse211/lab3/RobotDriver.java
@@ -106,7 +106,7 @@ public class RobotDriver {
             case EMERGENCY:
               // avoid the obstacle
               int angle = convertAngle(93.0);
-              int distance = convertDistance(2.0 / 3.0 * TILE_SIZE);
+              int distance = convertDistance(0.5 * TILE_SIZE);
 
               if (LorR()) {
                 // turn right

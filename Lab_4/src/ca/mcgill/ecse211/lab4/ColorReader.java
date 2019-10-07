2c747e01988d4d8218f98b00b431689a328a08ac
Y.P <yp.cheng@hotmail.ca>
2019-10-06 20:56:00 -0400

    color reader der

--- Lab_4/src/ca/mcgill/ecse211/lab4/ColorReader.java
+++ Lab_4/src/ca/mcgill/ecse211/lab4/ColorReader.java
@@ -4,29 +4,68 @@ import static ca.mcgill.ecse211.lab4.Resources.*;
 import lejos.robotics.SampleProvider;
 
 public class ColorReader implements Runnable {
-  private static final long SENSORTIMERlIMIT = 50;
 
-  // sensor
-  private SampleProvider sampleProvider = colorSensor.getRedMode();
-  private float[] sampleColor = new float[colorSensor.sampleSize()];
-  private float sample;
+	private boolean isFalling;
+	private static final long SENSORTIMERLIMIT = 50;
+	
+	public ColorReader(boolean isFalling) {
+		this.isFalling = isFalling;
+	}
 
-  public void run() {
-    long readingStart, readingEnd;
-    readingStart = System.currentTimeMillis();
+	// sensor
+	private SampleProvider sampleProvider = colorSensor.getRedMode();
+	private float[] sampleColor = new float[colorSensor.sampleSize()];
+	private float sample;
+	private static float der = 0;
+	private static float prev = 0;
+	private static int counter = 0;
 
-    sampleProvider.fetchSample(sampleColor, 0);
-    sample = sampleColor[0];
+	public void run() {
+		long readingStart, readingEnd;
+		readingStart = System.currentTimeMillis();
 
-    // this ensures reading
-    readingEnd = System.currentTimeMillis();
-    if (readingEnd - readingStart < SENSORTIMERlIMIT) {
-      Main.sleepFor(SENSORTIMERlIMIT - (readingEnd - readingStart));
-    }
-  }
+		sampleProvider.fetchSample(sampleColor, 0);
+		sample = sampleColor[0];
 
-  public float getSample() {
-    return sample;
-  }
+		// this ensures reading
+		readingEnd = System.currentTimeMillis();
+		if (readingEnd - readingStart < SENSORTIMERLIMIT) {
+			Main.sleepFor(SENSORTIMERLIMIT - (readingEnd - readingStart));
+		}
+	}
+
+	public float getSample() {
+		return sample;
+	}
+	
+	public boolean detectBlackLine() {
+		der = sample - prev;
+		prev = sample;
+		if (isFalling) {
+			if (der < 0 && counter < 2) {
+				counter++;
+				return false;
+			} else if (der < 0) {
+				counter = 0;
+				return true;
+			}
+			else {
+				counter = 0;
+				return false;
+			}
+		}
+		else {
+			if (der > 0 && counter < 2) {
+				counter++;
+				return false;
+			} else if (der < 0) {
+				counter = 0;
+				return true;
+			}
+			else {
+				counter = 0;
+				return false;
+			}
+		}
+	}
 }
-

e8ce99a74a15939d312445729c978f7beda3d1ff
Y.P <yp.cheng@hotmail.ca>
2019-10-21 17:10:52 -0400

    lab5

--- Lab_4/src/ca/mcgill/ecse211/lab4/Main.java
+++ Lab_4/src/ca/mcgill/ecse211/lab4/Main.java
@@ -5,35 +5,42 @@ import lejos.hardware.Button;
 
 public class Main {
   public static void main(String[] args) {
-    firstPhase();
+    try {
+		firstPhase();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
     secondPhase();
     System.exit(0);
   }
 
   @SuppressWarnings("deprecation")
-  public static void firstPhase() { //First phase when the robot uses the US sensor to align
+  public static void firstPhase() throws InterruptedException { //First phase when the robot uses the US sensor to align
     Thread a = new Thread(usPoller);
     Thread b = new Thread(alignmentDriver);
-    Thread c = new Thread(new AlignmentDriverDisplay());
+    //Thread c = new Thread(new AlignmentDriverDisplay());
     a.start();
     b.start();
-    c.start();
+   // c.start();
     Button.waitForAnyPress();
     UltrasonicPoller.kill = true;
     AlignmentDriverDisplay.kill = true;
-
+    a.join();
+    b.join();
+    //c.join();
   }
 
   public static void secondPhase() { //Second phase where the robot uses the color sensor to align
-    LCD.clear();
-    LCD.drawString("left: falling edge", 0, 0);
-    LCD.drawString("right: rising edge", 0, 1);
+//    LCD.clear();
+//    LCD.drawString("left: falling edge", 0, 0);
+//    LCD.drawString("right: rising edge", 0, 1);
     int res = Button.waitForAnyPress();
-    LCD.clear();
+ //   LCD.clear();
 
     // start the party
     new Thread(odometer).start();
-    new Thread(display).start();
+   // new Thread(display).start();
     
     new Thread(oc).start();
     new Thread(new ColorReader(res == Button.ID_LEFT)).start();
